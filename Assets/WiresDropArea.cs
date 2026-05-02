abb58ef6e25c370253b3c8f64592c3620c848b4b
svyatfurst <gromalex27@gmail.com>
2026-05-02 10:19:52 +0100

    Transferred tutorial room logic to new environment, added animations

--- Assets/WiresDropArea.cs
+++ Assets/WiresDropArea.cs
@@ -1,11 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 
 public class WiresDropArea : MonoBehaviour, ICardDropArea
 {
     public GameObject openDoor;
+    public Animator animator;
 
     public bool OnCardDropped(Card card)
     {
@@ -13,12 +12,22 @@ public class WiresDropArea : MonoBehaviour, ICardDropArea
 
         card.transform.position = transform.position;
 
-        //Changing the color of each red wire to red
+        // Changing the color of each red wire to green
         foreach (var sr in GameObject.Find("Power System/Wires/Red Wire").GetComponentsInChildren<SpriteRenderer>())
+        {
             sr.color = Color.green;
+        }
+
+        animator.SetBool("Play", true);
+        StartCoroutine(DelayAction());
+
+        return true;
+    }
+
+    IEnumerator DelayAction()
+    {
+        yield return new WaitForSeconds(1f);
 
         openDoor.SetActive(true);
-
-        return true; // accedpted card
     }
 }
